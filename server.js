0c35e72c5dfc54151c2b48a5b6f2fb7b60c1d416
Cole Rehling <colerehling@Coles-MBP.lan>
2025-01-22 22:29:34 -0600

    Switch to PostgreSQL

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ app.use(bodyParser.json());
 
 // MySQL Database Connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "ilikepie",
-    database: process.env.DB_NAME || "myt_entries",
+    host: process.env.DB_HOST ,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME ,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
